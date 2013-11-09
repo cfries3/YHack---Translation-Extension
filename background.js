a0bbbf567ce8b4bf907277d961eb7c8aaec3b339
Mike <mike@mike-Aspire-5755.(none)>
2013-11-09 03:27:34 -0500

    Made sure putting translated words back in would not alter html tags.

--- background.js
+++ background.js
@@ -1,15 +1,27 @@
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		var response = request.text;
-		var words = [ ["computer", "ordinateur"], ["memory", "memoria"], ["hardware", "hardware"] ];
+		var words = [ ["computer", "ordinateur"], ["memory", "memoria"], ["hardware", "hardware"], ["direct", "test"], ["table", "fail"] ];
 		for (var i = 0; i < words.length; i++) {
 			if (words[i][0] == words[i][1]) { 
 				continue; 
 			}
 			
-
-			while (response.search(words[i][0]) != -1) {
+			var uppercase = [ words[i][0].charAt(0).toUpperCase() + words[i][0].slice(1), words[i][1].charAt(0).toUpperCase() + words[i][1].slice(1) ]
+			
+			// First replace all tags by a marker $$$ and ^^^.
+			while (response.search("<" + words[i][0]) != -1 && response.search(words[i][0] + ">") != -1) {
+				response = response.replace("<" + words[i][0], "$$$");
+				response = response.replace(words[i][0] + ">", "^^^");
+			}
+			while (response.search(words[i][0]) != -1 && response.search(uppercase[0]) != -1) {
 				response = response.replace(words[i][0], "<span class='foreign'>" + words[i][1] + "</span>");
+				response = response.replace(uppercase[0], "<span class='foreign'>" + uppercase[1] + "</span>");
+			}
+			// Fix the tags
+			while (response.search("$$$") != -1 && response.search("^^^") != -1) {
+				response = response.replace("$$$", "<" + words[i][0]);
+				response = response.replace("^^^", words[i][0] + ">");
 			}
 		}
 		sendResponse({ translation: response });
