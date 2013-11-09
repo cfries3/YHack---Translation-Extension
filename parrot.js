727b00ab2fdd416625ba62cb0c9c1099bc714dc2
justindomingue <justin.domingue@hotmail.com>
2013-11-09 08:00:20 -0500

    Wolfram API URL now handles multiple languages

--- parrot.js
+++ parrot.js
@@ -4,14 +4,14 @@ function getTranslation(w, target) {
 
   $.ajax({
     dataType: "xml",
-    url: "http://api.wolframalpha.com/v2/query?appid=4KJG8A-T9U3UY2EPT&input=translate%20" + w + "%20from%20french%20to%20english&format=plaintext",
+    url: "http://api.wolframalpha.com/v2/query?appid=4KJG8A-T9U3UY2EPT&input=translate%20" + w + "%20from%20" + localStorage.native_language + "%20to%20" + localStorage.foreign_language + "&format=plaintext",
     data: null,
     success: function(data) {
      results = $(data).find("pod").last();
      $(target).children('.tooltip').text(results.find("plaintext").text().split(' ').splice(0,3).join(' ')); 
     },
     error: function(data) { 
-      console.log('ERROR');
+      console.log('Error');
     }
   });
 }
